4a4eaa94d8b8ad29f293092af1b1b0877efb973f
Zeyuan Li <zyli5313@gmail.com>
2013-07-17 00:09:11 -0700

    Add read from index.html

--- web.js
+++ web.js
@@ -3,8 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! From local!');
+  var str = fs.readFileSync('index.html', function (err, data) {
+    if (err) throw err;
+      console.log(data);
+  });
+  var buffer = new Buffer(str, "utf-8")
+  response.send(buffer.toString("utf-8"));
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
